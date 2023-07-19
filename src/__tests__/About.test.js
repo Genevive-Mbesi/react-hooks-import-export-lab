61b0ad1002ccb4e31934155c0d692eb779a7ae8c
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-07-19 21:31:49 +0300

    done with assignment

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -1,6 +1,5 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import About from "../components/About";
 
 test("it is exported as a default export", () => {
   try {
